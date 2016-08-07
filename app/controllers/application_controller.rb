a9ffc5fc8645f0dda57c563b3ec07fc90e01565e
Joshua Wetenkamp <dfjosh@me.com>
2016-08-07 16:20:50 -0700

    why can't I protect from forgery?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < JSONAPI::ResourceController
   # include JSONAPI::ActsAsResourceController
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session # why can't this be uncommented?!
 end
