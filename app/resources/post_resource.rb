be9e532bf6ee024796b6d21d9e5e69ef9d7b37c6
Joshua Wetenkamp <dfjosh@me.com>
2016-08-11 00:18:15 -0700

    adds pagination, default sort, and filter

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -1,6 +1,12 @@
 class PostResource < JSONAPI::Resource
   attributes :title, :author, :body, :created_at, :updated_at
   has_many :categories
-  has_many :tags, always_include_linkage_data: true
+  has_many :tags#, always_include_linkage_data: true
   has_many :comments
+
+  def self.default_sort
+    [{ field: 'created_at', direction: :desc }]
+  end
+
+  filter :author
 end
