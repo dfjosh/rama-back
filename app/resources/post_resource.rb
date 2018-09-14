ace7f4bef56a5964c6f17dc19888a58ece552ac6
Joshua Wetenkamp <dfjosh@me.com>
2018-09-13 19:48:10 -0700

    custom filter

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -9,6 +9,10 @@ class PostResource < JSONAPI::Resource
   end
 
   filters :author, :categories, :title
+  
+  filter "!categories", apply: ->(records, value, _options) {
+    records.joins(:categories).where("categories.id NOT IN (?)", value[0])
+  }
 
   paginator :paged
 
