d76f5886909e6521681d534f71c900455ee1d4b7
Joshua Wetenkamp <dfjosh@me.com>
2018-12-17 16:10:46 -0800

    dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-  has_many :post_categories
+  has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
