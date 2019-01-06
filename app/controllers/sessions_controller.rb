14019d99698d4ac4037a614317efdf7e6da09e96
Joshua Wetenkamp <dfjosh@me.com>
2019-01-05 17:02:37 -0800

    clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,21 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:username])
     if user&.authenticate(params[:password])
       puts "Authenticated!"
-      session[:user_id] = user.id
+      # session[:user_id] = user.id
       render json: {access_token: SecureRandom.hex(32)}
     else
       render json: {error: "invalid email or password."}
     end
   end
   
-  def destroy
-    session[:user_id] = nil
-    render json: {message: "logged out."}
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   render json: {message: "logged out."}
+  # end
   
   private
   
-  def session_params
-    params.require(:data).require(:attributes).permit(:email, :password)
-  end
+  # def session_params
+  #   params.require(:data).require(:attributes).permit(:email, :password)
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,2 @@
 class UsersController < ApplicationController
-  def new
-  end
-  
-  def create
-  end
 end
