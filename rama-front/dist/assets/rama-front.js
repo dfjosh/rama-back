d24c4a218f8eaf9566b0ed07302fdee30bfc8834
Joshua Wetenkamp <dfjosh@me.com>
2019-03-24 12:30:26 -0700

    fixes knock auth

--- rama-front/dist/assets/rama-front.js
+++ rama-front/dist/assets/rama-front.js
@@ -1380,7 +1380,7 @@
 ;define('rama-front/config/environment', [], function() {
   
           var exports = {
-            'default': {"modulePrefix":"rama-front","environment":"development","rootURL":"/","locationType":"router-scroll","historySupportMiddleware":true,"apiURL":"http://localhost:3000/api","cdnURL":"http://localhost:3000","EmberENV":{"FEATURES":{},"EXTEND_PROTOTYPES":{"Date":false}},"APP":{"name":"rama-front","version":"0.0.0+31787cf1"},"ember-simple-auth-token":{"serverTokenEndpoint":"http://localhost:3000/api/user_token/","tokenPropertyName":"token","refreshTokenPropertyName":"token"},"emberData":{"enableRecordDataRFCBuild":false},"exportApplicationGlobal":true}
+            'default': {"modulePrefix":"rama-front","environment":"development","rootURL":"/","locationType":"router-scroll","historySupportMiddleware":true,"apiURL":"http://localhost:3000/api","cdnURL":"http://localhost:3000","EmberENV":{"FEATURES":{},"EXTEND_PROTOTYPES":{"Date":false}},"APP":{"name":"rama-front","version":"0.0.0+54acb288"},"ember-simple-auth-token":{"serverTokenEndpoint":"http://localhost:3000/api/user_token/","tokenPropertyName":"token","refreshTokenPropertyName":"token"},"emberData":{"enableRecordDataRFCBuild":false},"exportApplicationGlobal":true}
           };
           Object.defineProperty(exports, '__esModule', {value: true});
           return exports;
@@ -1389,7 +1389,7 @@
 
 ;
           if (!runningTests) {
-            require("rama-front/app")["default"].create({"name":"rama-front","version":"0.0.0+31787cf1"});
+            require("rama-front/app")["default"].create({"name":"rama-front","version":"0.0.0+54acb288"});
           }
         
 //# sourceMappingURL=rama-front.map
