da2ce57e79b06953d3626334a51611c6972a05bb
Joshua Wetenkamp <dfjosh@me.com>
2019-03-29 23:35:50 -0700

    get the post slugs

--- db/data/20180903234713_set_project_post_images.rb
+++ db/data/20180903234713_set_project_post_images.rb
@@ -1,62 +1,62 @@
 class SetProjectPostImages < ActiveRecord::Migration[5.2]
   PROJECT_POSTS = [
     {
-      id: 110,
+      slug: "dartcalc",
       img: "/uploads/2015/07/Dartboard2.jpg",
       url: "http://distantfuturejosh.com/dartcalc/dartCalc.py"
     },
     {
-      id: 109,
+      slug: "mars-longform",
       img: "/uploads/2014/10/PSP_001660_2570.jpg",
       url: "http://distantfuturejosh.com/marslongform/"
     },
     {
-      id: 108,
+      slug: "land-jacks",
       img: "/uploads/2014/04/flags1.jpg",
       url: "http://distantfuturejosh.com/landjacks"
     },
     {
-      id: 107,
+      slug: "destroy-all-sites",
       img: "/uploads/2014/03/destroyallsites.jpg",
       url: "http://distantfuturejosh.com/destroyallsites"
     },
     {
-      id: 106,
+      slug: "kung-fu",
       img: "/uploads/2014/02/kung-fu-post.jpg",
       url: "http://distantfuturejosh.com/kungfu"
     },
     {
-      id: 105,
+      slug: "endomoons",
       img: "/uploads/2013/12/endomoons.jpg",
       url: "http://distantfuturejosh.com/endomoons"
     },
     {
-      id: 103,
+      slug: "colorly",
       img: "/uploads/2013/11/colorly_huge.jpg",
       url: "http://distantfuturejosh.com/colorly"
     },
     {
-      id: 102,
+      slug: "scale-solar-system",
       img: "/uploads/2013/11/scalesolarsystem_huge.jpg",
       url: "http://distantfuturejosh.com/scalesolarsystem"
     },
     {
-      id: 101,
+      slug: "books",
       img: "/uploads/2013/09/dark_huge.jpg",
       url: "http://distantfuturejosh.com/books/dark.html"
     },
     {
-      id: 100,
+      slug: "the-solar-system-at-closest-approach",
       img: "/uploads/2013/09/solarsystem_huge.jpg",
       url: "http://distantfuturejosh.com/books/solarsystem.html"
     } ,
     {
-      id: 80,
+      slug: "design",
       img: "/uploads/2013/11/design_huge.jpg",
       url: "http://distantfuturejosh.com/playground/design"
     },
     {
-      id: 20,
+      slug: "no-format-podcast",
       img: "/uploads/2013/12/noformat_huge2.jpg",
       url: "http://distantfuturejosh.com/noformat"
     }
@@ -65,7 +65,7 @@ class SetProjectPostImages < ActiveRecord::Migration[5.2]
   def up
     Post.reset_column_information
     PROJECT_POSTS.each do |post_hash|
-      post = Post.find(post_hash[:id])
+      post = Post.find_by_slug(post_hash[:slug])
       post.feature_image = post_hash[:img]
       post.feature_link = post_hash[:url]
       post.save!
