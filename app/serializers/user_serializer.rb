8bd882e419662c2100ab764abe50a5d303891d62
Joshua Wetenkamp <dfjosh@me.com>
2019-05-12 22:28:17 -0700

    transaction, pen_name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   # set_key_transform :dash
 
-  attributes :first_name, :last_name, :email, :is_admin
+  attributes :first_name, :last_name, :email, :is_admin, :pen_name
 end
